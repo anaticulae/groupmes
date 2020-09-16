3209e281d112f51f479089acffc5d6711783b68b
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-09-16 16:59:43 +0200

    test(footer): add test to exclude page number from footer

--- tests/groupme_/footnotes/test_negative.py
+++ tests/groupme_/footnotes/test_negative.py
@@ -23,3 +23,15 @@ def test_footer_paper18_page3(testdir, monkeypatch):
     page3 = utila.select_page(extracted, 3)
     # do not interpret this formula is footer
     assert not page3.footer
+
+
+def test_do_not_merge_pagenumber_footer_bachelor76_page21(testdir, monkeypatch):
+    extracted = tests.groupme_.footerheader.extractor.footer(
+        power.BACHELOR076_PDF,
+        testdir,
+        monkeypatch,
+        pages=21,
+    )
+    note = utila.select_page(extracted, 21).footer.notes[0]
+    note = note.text.strip()  # TODO: remove strip later
+    assert note.endswith('Vgl. Schlick, J. et. al.(2014), S.58f.'), str(note)
