343ccc23346c69c3997b70b3a093502084134871
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-10-20 15:55:37 +0200

    patch(utila): patch three side equal cluster

--- groupme/__patch__.py
+++ groupme/__patch__.py
@@ -6,3 +6,37 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
+
+import utila
+
+
+def three_side_equal_cluster(
+        todo,
+        min_elements: int = 2,
+        max_diff=2.0,
+        selector=None,
+):
+    selector = selector if selector else lambda x: x[0]
+
+    def classifier(candidat, clusteritem):
+
+        def matcher(candidat, clusteritem):
+            bounding_cluster = selector(clusteritem)
+            bounding_test = selector(candidat)
+
+            eqaul = sum([
+                utila.near(first, second, diff=max_diff)
+                for (first, second) in zip(bounding_test, bounding_cluster)
+            ])
+            return eqaul >= 3
+
+        return matcher(candidat, clusteritem)
+
+    return utila.classifier.base.determine_cluster(
+        todo,
+        classifier,
+        min_elements=min_elements,
+    )
+
+
+utila.three_side_equal_cluster = three_side_equal_cluster
