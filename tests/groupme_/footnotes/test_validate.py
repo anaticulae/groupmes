7a1b640627250478c1b4bdc74398d8999007d5f3
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-11-23 14:02:11 +0100

    test(refactor): use improved baseliner

--- tests/groupme_/footnotes/test_validate.py
+++ tests/groupme_/footnotes/test_validate.py
@@ -7,18 +7,19 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
+import functools
 import os
 
 import power
 import pytest
 import serializeraw
 import utila
+import utilatest
 
 import groupme
 import tests.groupme_
 
-EXPECTED = os.path.join(groupme.ROOT, 'tests/groupme_/footnotes/expected')
-file_read = lambda x: utila.file_read(os.path.join(EXPECTED, x)).strip()  # pylint:disable=C0103
+ARCHIVE = os.path.join(groupme.ROOT, 'tests/groupme_/footnotes/expected')
 
 
 # yapf:disable
@@ -30,20 +31,42 @@ file_read = lambda x: utila.file_read(os.path.join(EXPECTED, x)).strip()  # pyli
     pytest.param(power.BACHELOR037_PDF, None, 'bachelor037', id='bachelor037'),
 ])
 # yapf:enable
-# @utilatest.nightly
 def test_footnotes_validate(source, pages, expected, testdir, monkeypatch):
-    pages = '' if pages is None else f'--pages={pages}'
-    cmd = f'-i {power.link(source)}  --footer {pages}'
-    tests.groupme_.run(cmd, monkeypatch=monkeypatch)
-    # verify
-    loaded = serializeraw.load_footnotes(testdir.tmpdir)
-    footnotes = plain(loaded)
-    expected = file_read(expected)
-    assert footnotes == expected
+    Evaluate(
+        source=source,
+        pages=pages,
+        expected=expected,
+        workdir=testdir.tmpdir,
+        monkeypatch=monkeypatch,
+    ).evaluate()
 
 
-def plain(footnotes) -> str:
-    footnotes = utila.flatten_content(footnotes)
-    footnotes = [utila.normalize_text(item.text.strip()) for item in footnotes]
-    result = utila.NEWLINE.join(footnotes)
-    return result
+class Evaluate(utilatest.BaseLiner):
+
+    def __init__(self, source, pages, expected, workdir, monkeypatch):
+        super().__init__(
+            program=functools.partial(
+                tests.groupme_.run,
+                monkeypatch=monkeypatch,
+            ),
+            step='footer',
+            pages=pages,
+            source=power.link(source),
+            workdir=workdir,
+            archive=ARCHIVE,
+            loader=self.load_footnotes,
+            convert_source=False,
+            index=expected,
+        )
+
+    def load_footnotes(self, _):  # pylint:disable=W0613
+        loaded = serializeraw.load_footnotes(self.workdir)
+        return loaded
+
+    def raw(self, value) -> str:
+        footnotes = utila.flatten_content(value)
+        footnotes = [
+            utila.normalize_text(item.text.strip()) for item in footnotes
+        ]
+        result = utila.NEWLINE.join(footnotes)
+        return result
