9c6a199a7091d880fd39ed1ec3e3ae4a7eb61923
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-11-26 16:00:05 +0100

    test(refactor): shorten validation code

--- tests/groupme_/toc/test_validate.py
+++ tests/groupme_/toc/test_validate.py
@@ -23,28 +23,23 @@ ARCHIVE = os.path.join(groupme.ROOT, 'tests/groupme_/toc/expected')
 TEN = utila.make_tuple(10)
 
 
-# yapf:disable, format the list by hand
 @pytest.mark.parametrize('source, pages', [
-    pytest.param(power.BACHELOR076_PDF, TEN, id='bachelor76'),
-    pytest.param(power.BACHELOR111_PDF, (1, 2, 3, 4), id='bachelor111',
-                marks=pytest.mark.xfail(reason='literaturverzeichnis sub notes')),
-    pytest.param(power.BACHELOR241_PDF, (4, 5, 6, 7), id='bachelor241',
-                marks=pytest.mark.xfail(reason='literaturverzeichnis sub notes')),
-    pytest.param(power.HOME050_PDF, (3, 4), id='homework50'),
-    pytest.param(power.MASTER083_PDF, TEN, id='master83'),
-    pytest.param(power.MASTER089_PDF, TEN, id='master89'),
-    pytest.param(power.MASTER098_PDF, TEN, id='master98'),
-    pytest.param(power.MASTER099_PDF, TEN, id='master99'),
-    pytest.param(power.MASTER072_PDF, None, id='master72'),
-    pytest.param(power.BACHELOR090_PDF, TEN, id='bachelor90',
-                marks=pytest.mark.xfail(reason='investigate later')),
-    pytest.param(power.BACHELOR063_PDF, TEN, id='bachelor63'),
-    pytest.param(power.MASTER078_PDF, TEN, id='master78'),
-    pytest.param(power.DISS180_PDF, (4, 5), id='diss180'),
-    pytest.param(power.DISS157_PDF, (6,7,8), id='diss157'),
+    utilatest.step(power.BACHELOR063_PDF, TEN),
+    utilatest.step(power.BACHELOR076_PDF, TEN),
+    utilatest.step(power.BACHELOR090_PDF, TEN, reason='investigate laster'),
+    utilatest.step(power.BACHELOR111_PDF, (1, 2, 3, 4), reason='sub notes'),
+    utilatest.step(power.BACHELOR241_PDF, (4, 5, 6, 7), reason='sub notes'),
+    utilatest.step(power.DISS157_PDF, (6, 7, 8)),
+    utilatest.step(power.DISS180_PDF, (4, 5)),
+    utilatest.step(power.HOME050_PDF, (3, 4)),
+    utilatest.step(power.MASTER072_PDF, None),
+    utilatest.step(power.MASTER078_PDF, TEN),
+    utilatest.step(power.MASTER083_PDF, TEN),
+    utilatest.step(power.MASTER089_PDF, TEN),
+    utilatest.step(power.MASTER098_PDF, TEN),
+    utilatest.step(power.MASTER099_PDF, TEN),
 ])
-# yapf:enable
-@utilatest.nightly
+# @utilatest.nightly
 def test_toc_validate(source, pages, monkeypatch, testdir):
     """Verify parsing behavior and check that toc is located
     automatically in range of `TEN` pages."""
