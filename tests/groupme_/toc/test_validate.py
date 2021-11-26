f4336e553bc80655883da7b7def706ac07a20cdc
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-11-26 13:58:34 +0100

    test(refactor): replace with utilatest evaluator
    + rename file

--- tests/groupme_/toc/test_validate.py
+++ tests/groupme_/toc/test_validate.py
@@ -7,9 +7,9 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
+import functools
 import os
 
-import iamraw
 import power
 import pytest
 import serializeraw
@@ -17,69 +17,69 @@ import utila
 import utilatest
 
 import groupme
-import groupme.path
 import tests
 
-EXPECTED = os.path.join(groupme.ROOT, 'tests/groupme_/toc/expected')
-file_read = lambda x: utila.file_read(os.path.join(EXPECTED, x)).strip()  # pylint:disable=C0103
-
-
-def merge_required(toc: iamraw.Toc) -> str:
-    result = []
-
-    def recursive(item, level):
-        result = []
-        result.append('    ' * level + item.title)
-        assert item.raw_location >= 0, str(item)
-        if item.children:
-            for child in item.children:
-                result.extend(recursive(child, level + 1))
-        return result
-
-    for item in toc:
-        result.extend(recursive(item, level=0))
-    titles = utila.NEWLINE.join(result)
-    return titles
-
-
+ARCHIVE = os.path.join(groupme.ROOT, 'tests/groupme_/toc/expected')
 TEN = utila.make_tuple(10)
 
 
 # yapf:disable, format the list by hand
-@pytest.mark.parametrize('source, validate, pages', [
-    pytest.param(power.BACHELOR076_PDF, 'bachelor076', TEN, id='bachelor76'),
-    pytest.param(power.BACHELOR111_PDF, 'bachelor111', (1, 2, 3, 4), id='bachelor111',
-                 marks=pytest.mark.xfail(reason='literaturverzeichnis sub notes')),
-    pytest.param(power.BACHELOR241_PDF, 'bachelor241', (4, 5, 6, 7), id='bachelor241',
-                 marks=pytest.mark.xfail(reason='literaturverzeichnis sub notes')),
-    pytest.param(power.HOME050_PDF, 'homework050', (3, 4), id='homework50'),
-    pytest.param(power.MASTER083_PDF, 'master083', TEN, id='master83'),
-    pytest.param(power.MASTER089_PDF, 'master089', TEN, id='master89'),
-    pytest.param(power.MASTER098_PDF, 'master098', TEN, id='master98'),
-    pytest.param(power.MASTER099_PDF, 'master099', TEN, id='master99'),
-    pytest.param(power.MASTER072_PDF, 'master072', None, id='master72'),
-    pytest.param(power.BACHELOR090_PDF, 'bachelor090', TEN, id='bachelor90',
-            marks=pytest.mark.xfail(reason='investigate later')),
-    pytest.param(power.BACHELOR063_PDF, 'bachelor063', TEN, id='bachelor63'),
-    pytest.param(power.MASTER078_PDF, 'master078', TEN, id='master78'),
-    pytest.param(power.DISS180_PDF, 'diss180', (4, 5), id='diss180'),
-    pytest.param(power.DISS157_PDF, 'diss157', (6, 7, 8), id='diss157'),
+@pytest.mark.parametrize('source, pages', [
+    pytest.param(power.BACHELOR076_PDF, TEN, id='bachelor76'),
+    pytest.param(power.BACHELOR111_PDF, (1, 2, 3, 4), id='bachelor111',
+                marks=pytest.mark.xfail(reason='literaturverzeichnis sub notes')),
+    pytest.param(power.BACHELOR241_PDF, (4, 5, 6, 7), id='bachelor241',
+                marks=pytest.mark.xfail(reason='literaturverzeichnis sub notes')),
+    pytest.param(power.HOME050_PDF, (3, 4), id='homework50'),
+    pytest.param(power.MASTER083_PDF, TEN, id='master83'),
+    pytest.param(power.MASTER089_PDF, TEN, id='master89'),
+    pytest.param(power.MASTER098_PDF, TEN, id='master98'),
+    pytest.param(power.MASTER099_PDF, TEN, id='master99'),
+    pytest.param(power.MASTER072_PDF, None, id='master72'),
+    pytest.param(power.BACHELOR090_PDF, TEN, id='bachelor90',
+                marks=pytest.mark.xfail(reason='investigate later')),
+    pytest.param(power.BACHELOR063_PDF, TEN, id='bachelor63'),
+    pytest.param(power.MASTER078_PDF, TEN, id='master78'),
+    pytest.param(power.DISS180_PDF, (4, 5), id='diss180'),
+    pytest.param(power.DISS157_PDF, (6,7,8), id='diss157'),
 ])
 # yapf:enable
 @utilatest.nightly
-def test_toc_validate(source, validate, pages, monkeypatch, testdir):
+def test_toc_validate(source, pages, monkeypatch, testdir):
     """Verify parsing behavior and check that toc is located
     automatically in range of `TEN` pages."""
-    source = power.link(source)
-    pages = utila.from_tuple(pages, separator=',') if pages else ':'
-    cmd = f'-i {source} --toc --pages={pages}'
-    tests.groupme_.run(cmd, monkeypatch=monkeypatch)
+    pages = utila.from_tuple(pages, ',') if pages else ':'
+    Evaluate(source, pages, testdir.tmpdir, monkeypatch).evaluate()
 
-    toc = serializeraw.load_toc(testdir.tmpdir)
 
-    if callable(validate):
-        validate(toc)
-        return
-    validate = file_read(validate)
-    titles = merge_required(toc)
-    assert titles == validate, validate
+class Evaluate(utilatest.BaseLiner):
+
+    def __init__(self, source, pages, workdir, monkeypatch):
+        super().__init__(
+            program=functools.partial(
+                tests.groupme_.run,
+                monkeypatch=monkeypatch,
+            ),
+            step='toc',
+            source=source,
+            pages=pages,
+            workdir=workdir,
+            archive=ARCHIVE,
+            loader=serializeraw.load_toc,
+        )
+
+    def raw(self, value) -> str:
+        result = []
+        for item in value:
+            result.extend(self.recursive(item, level=0))
+        titles = utila.NEWLINE.join(result)
+        return titles
+
+    def recursive(self, item, level):
+        result = ['    ' * level + item.title]
+        assert item.raw_location >= 0, str(item)
+        if not item.children:
+            return result
+        for child in item.children:
+            result.extend(self.recursive(child, level + 1))
+        return result
