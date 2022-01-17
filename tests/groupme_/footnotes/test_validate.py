59e8139e7762b476d5affec7a5f91200b0b462c7
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-01-17 19:38:06 +0100

    test(footnotes): validate master127

--- tests/groupme_/footnotes/test_validate.py
+++ tests/groupme_/footnotes/test_validate.py
@@ -33,6 +33,7 @@ ARCHIVE = os.path.join(groupme.ROOT, 'tests/groupme_/footnotes/expected')
     pytest.param(power.DISS178_PDF, '0:30', 'diss178', id='diss178'),
     pytest.param(power.DISS406_PDF, '0:50', 'diss406', id='diss406'),
     pytest.param(power.DISS273_PDF, '30:60', 'diss273', id='diss273'),
+    pytest.param(power.MASTER127_PDF, ':', 'master127', id='master127'),
 ])
 # yapf:enable
 @utilatest.longrun
