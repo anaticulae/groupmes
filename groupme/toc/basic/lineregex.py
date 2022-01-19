19b7301d1623116f111a2348b01fa693bc5bc0fb
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-01-19 18:35:12 +0100

    format(style): format regex

--- groupme/toc/basic/lineregex.py
+++ groupme/toc/basic/lineregex.py
@@ -36,17 +36,19 @@ def parse(line: str) -> groupme.toc.TocLine:
 
 LEVEL_DOTTED_OPTIONAL = r'(?P<level>(\d{1,2}\.?){1,3}\d{0,2})'
 
-LEVEL_LETTER = r"""(?P<level>
-                    (
-                    (A|B|C|D|E|F|G)(\.?|\))|
-                    (AA|BB|CC|DD|EE|FF|GG)\)|
-                    (I|II|III|IV|V|VI|VII|VIII)(\)|\.)|
-                    (A|B|C|D)\.(\d{1,2}\.?)+\d{0,2}|
-                    KAPITEL[ ]\d{1,2}|
-                    (SECTION|PART)[ ]{0,3}\d{1,2}:|
-                    Anhang[ ](A|B|C|D)\: # TODO: Exclude Anhang and :
-                    )
-                )"""
+LEVEL_LETTER = r"""
+    (?P<level>
+        (
+            (A|B|C|D|E|F|G)(\.?|\))|
+            (AA|BB|CC|DD|EE|FF|GG)\)|
+            (I|II|III|IV|V|VI|VII|VIII)(\)|\.)|
+            (A|B|C|D|E|F|G)\.(\d{1,2}\.?)+\d{0,2}|
+            (KAPITEL|CHAPTER)[ ]\d{1,2}|
+            (SECTION|PART)[ ]{0,3}\d{1,2}:|
+            (ANHANG|APPENDIX)[ ](A|B|C|D)\: # TODO: Exclude Anhang and :
+        )
+    )
+"""
 
 USER_CHARACTER = [
     r'\w\d\(\)\-\.\[\]',
