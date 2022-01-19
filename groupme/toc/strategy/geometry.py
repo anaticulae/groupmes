58ba4d03a5570b0d6ee90d3a9325b5b58ce01f39
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-01-19 18:06:02 +0100

    refactor(utils): improve naming

--- groupme/toc/strategy/geometry.py
+++ groupme/toc/strategy/geometry.py
@@ -14,7 +14,7 @@ import configo
 import texmex
 import utila
 
-import groupme.toc.basic.utils
+import groupme.toc.basic.group
 import groupme.toc.strategy
 
 HEADLINE_LEVEL_MAX = configo.HV_INT_PLUS(default=3)
@@ -30,7 +30,7 @@ class GeometryTocExtractor(groupme.toc.strategy.ExtractorStrategy):
 
         grouped = group_areas(extracted)
         content = [
-            groupme.toc.basic.utils.parse_group(group, page)
+            groupme.toc.basic.group.parse_group(group, page)
             for page, group in grouped
         ]
         # remove empty
--- groupme/toc/strategy/georegex.py
+++ groupme/toc/strategy/georegex.py
@@ -26,7 +26,7 @@ import configo
 import texmex
 import utila
 
-import groupme.toc.basic.utils
+import groupme.toc.basic.group
 import groupme.toc.strategy
 
 GROUP_GAP_MIN = configo.HV_FLOAT_PLUS(default=30.0)
@@ -56,7 +56,7 @@ def analyse_page(content: texmex.PageTextNavigator):
     content = groupme.toc.strategy.remove_headline(content)
     grouped = group_areas(content)
     result = [
-        groupme.toc.basic.utils.parse_group(items, content.page)
+        groupme.toc.basic.group.parse_group(items, content.page)
         for items in grouped
     ]
     # remove not parsed
