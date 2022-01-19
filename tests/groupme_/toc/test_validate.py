d97857c514a5e9d82ecbdcaa25a725f7627f7ec4
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-01-19 17:22:00 +0100

    test(toc): validate master155

--- tests/groupme_/toc/test_validate.py
+++ tests/groupme_/toc/test_validate.py
@@ -41,6 +41,7 @@ TEN = utila.make_tuple(10)
     utilatest.step(power.DISS406_PDF, (3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)),
     utilatest.step(power.MASTER099B_PDF, (2,)),
     utilatest.step(power.MASTER049_PDF, (4,)),
+    utilatest.step(power.MASTER155_PDF, (1, 2)),
 ])
 @utilatest.nightly
 def test_toc_validate(source, pages, monkeypatch, testdir):
