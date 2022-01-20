2d7963bb39c8f0365b408b1b11a1aa903c079e88
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-01-20 17:25:20 +0100

    test(toc): validate diss172

--- tests/groupme_/toc/test_validate.py
+++ tests/groupme_/toc/test_validate.py
@@ -48,7 +48,7 @@ TEN = utila.make_tuple(10)
     utilatest.step(power.MASTER155_PDF, (1, 2)),
     utilatest.step(power.BACHELOR128_PDF, (3, 4, 5)),
     utilatest.step(power.MASTER193_PDF, (3, 4, 5)),
-    utilatest.step(power.DISS172_PDF, (7,8)),
+    utilatest.step(power.DISS172_PDF, (7, 8)),
 ])
 @utilatest.nightly
 def test_toc_validate(source, pages, monkeypatch, testdir):
