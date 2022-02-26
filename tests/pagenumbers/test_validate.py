c585306225f0be6a2b1937e317150d7219d57488
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-02-26 18:59:30 +0100

    test(pagenumbers): validate diss287

--- tests/pagenumbers/test_validate.py
+++ tests/pagenumbers/test_validate.py
@@ -62,6 +62,20 @@ def diss218(result):
     assert utila.isascending(current)
 
 
+def diss287(result):
+    current = [item.detected for item in result]
+    expected = [
+        'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI',
+        'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+        28, 29, 30, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+        48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65,
+        66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80
+    ]
+    assert current == expected
+    assert len(current) == 78
+
+
 HUNDRED = utila.ranged_list(100)
 
 
@@ -76,6 +90,7 @@ HUNDRED = utila.ranged_list(100)
     pytest.param(power.DISS218_PDF, HUNDRED, diss218, id='diss218'),
     pytest.param(power.MASTER049_PDF, None, master049, id='master049'),
     pytest.param(power.DISS480_PDF, None, diss480, id='diss480'),
+    pytest.param(power.DISS287_PDF, None, diss287, id='diss287'),
 ])
 @utilatest.nightly
 def test_validate_pagenumbers(source, pages, expected):
