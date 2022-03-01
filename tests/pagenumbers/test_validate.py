7ab49ed42f83c256cd9ae3af6c59e314ef414469
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-03-01 01:31:41 +0100

    test(pagenumbers): validate page number diss170b

--- tests/pagenumbers/test_validate.py
+++ tests/pagenumbers/test_validate.py
@@ -75,6 +75,25 @@ def diss148(result):
     assert current == expected
 
 
+def diss170b(result):
+    current = [item.detected for item in result]
+    # TODO: MISSING PAGE 144 and PAGE 145
+    expected = [
+        'I', 'II', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+        18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+        36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
+        54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
+        72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
+        90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105,
+        106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119,
+        120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133,
+        134, 135, 136, 137, 138, 139, 140, 141, 142, 145, 146, 147, 148, 149,
+        150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163,
+        164, 165, 166, 167, 168
+    ]
+    assert current == expected
+
+
 def diss480(result):
     current = [item.detected for item in result]
     assert utila.isascending(current)
@@ -106,6 +125,7 @@ HUNDRED = utila.ranged_list(100)
     pytest.param(power.BACHELOR085_PDF, None, bachelor085, id='bachelor085'),
     pytest.param(power.BACHELOR111_PDF, None, bachelor111, id='bachelor111'),
     pytest.param(power.DISS148_PDF, None, diss148, id='diss148'),
+    pytest.param(power.DISS170B_PDF, None, diss170b, id='diss170b'),
     pytest.param(power.DISS218_PDF, HUNDRED, diss218, id='diss218'),
     pytest.param(power.DISS287_PDF, None, diss287, id='diss287'),
     pytest.param(power.DISS406_PDF, None, 119, id='diss406'),
