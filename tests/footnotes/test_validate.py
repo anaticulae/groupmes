0adca34d5d040582d23e832962933488641da98d
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-04-07 17:34:08 +0200

    test(footnotes): validate bachelor101

--- tests/footnotes/test_validate.py
+++ tests/footnotes/test_validate.py
@@ -40,6 +40,7 @@ step = lambda x: pytest.param(x, ':', utila.file_name(x), id=utila.file_name(x))
     step(power.BACHELOR063_PDF),
     step(power.BACHELOR078_PDF),
     step(power.BACHELOR086_PDF),
+    step(power.BACHELOR101_PDF),
     step(power.BACHELOR111_PDF),
     step(power.DISS143_PDF),
     step(power.DISS172_PDF),
