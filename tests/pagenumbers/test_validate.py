eebfae4d3989d10e5a5b2745f1014c8521917956
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-06-14 19:06:02 +0200

    test(pagenumbers): convert unit test

--- tests/pagenumbers/test_validate.py
+++ tests/pagenumbers/test_validate.py
@@ -9,7 +9,6 @@
 
 import functools
 
-import iamraw
 import power
 import pytest
 import serializeraw
@@ -17,143 +16,29 @@ import utila
 import utilatest
 
 import groupme
-import groupme.feature.pagenumbers
 import tests
 
-ARCHIVE = utila.join(groupme.ROOT, 'tests/pagenumbers/expected')
-
-
-def bachelor085(result):  # pylint:disable=W0613
-    """Enable after fixing rawmaker text extractor."""
-    return
-    # current = [item.detected for item in result]
-    # expected = []
-    # assert current == expected
-    # assert len(current) == 78
-
-
-def bachelor111(result):
-    assert len(result) == 110
-    expected = 'i ii iii iv'.split() + utila.ranged_list(start=1, end=107)
-    current = [item.detected for item in result]
-    assert current == expected
-
-
-def master110(result):
-    # VALIDATED: master110: 110-15?
-    assert len(result) == 91
-    current = [item.detected for item in result]
-    # master110: 86 and xii is currently not detected
-    expected = [
-        'iii', 'v', 'vii', 'ix', 'xi', 'xiii', 'xv', 'xvii', 1, 2, 3, 5, 6, 7,
-        8, 9, 11, 12, 13, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
-        28, 29, 30, 31, 32, 33, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46,
-        47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 65,
-        66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 81, 82, 83, 85,
-        87, 88, 89, 90, 91
-    ]
-    assert current == expected
-
-
-def master049(result):
-    assert len(result) == 45
-    current = [item.detected for item in result]
-    expected = [
-        'ii', 'iii', 'iv', 'v', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-        15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
-        33, 34, 35, 36, 37, 38, 39, 40, 41
-    ]
-    assert current == expected
-
-
-def diss148(result):
-    """This document contains a lot of paper as a part accumulative diss."""
-    current = [item.detected for item in result]
-    expected = [
-        'i', 'ii', 'iii', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-        17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
-        35, 43, 2, 3, 4, 5, 49, 15839, 15840, 15841, 15842, 15843, 15844, 15845,
-        15846, 62, 78, 79, 80, 81, 82, 83, 84, 85, 79, 106, 107, 108, 109, 110,
-        111, 112, 113, 114, 115, 117, 118, 119, 120, 121, 122, 123, 124, 125,
-        126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137
-    ]
-    assert current == expected
-
-
-def diss170b(result):
-    current = [item.detected for item in result]
-    # TODO: MISSING PAGE 144 and PAGE 145
-    expected = [
-        'I', 'II', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
-        18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
-        36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
-        54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
-        72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
-        90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105,
-        106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119,
-        120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133,
-        134, 135, 136, 137, 138, 139, 140, 141, 142, 145, 146, 147, 148, 149,
-        150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163,
-        164, 165, 166, 167, 168
-    ]
-    assert current == expected
-
-
-def diss480(result):
-    current = [item.detected for item in result]
-    assert utila.isascending(current)
-
-
-def diss218(result):
-    current = [item.detected for item in result]
-    assert utila.isascending(current)
-
-
-HUNDRED = utila.ranged_list(100)
-
-
-@pytest.mark.parametrize('source,pages,expected', [
-    pytest.param(power.BACHELOR085_PDF, None, bachelor085, id='bachelor085'),
-    pytest.param(power.BACHELOR111_PDF, None, bachelor111, id='bachelor111'),
-    pytest.param(power.DISS148_PDF, None, diss148, id='diss148'),
-    pytest.param(power.DISS170B_PDF, None, diss170b, id='diss170b'),
-    pytest.param(power.DISS218_PDF, HUNDRED, diss218, id='diss218'),
-    pytest.param(power.DISS406_PDF, None, 119, id='diss406'),
-    pytest.param(power.DISS480_PDF, None, diss480, id='diss480'),
-    pytest.param(power.MASTER049_PDF, None, master049, id='master049'),
-    pytest.param(power.MASTER072_PDF, None, 69, id='master72pages'),
-    pytest.param(power.MASTER091A_PDF, None, 88, id='master91a'),
-    pytest.param(power.MASTER110_PDF, None, master110, id='master110'),
-    pytest.param(power.MASTER127_PDF, None, 127 - 1, id='master127'),
-])
-@utilatest.nightly
-def test_validate_pagenumbers(source, pages, expected):
-    extracted = power.link(source)
-    # TODO: bottom only, add header page extraction
-    # run extractor
-    result = groupme.feature.pagenumbers.work(
-        iamraw.path.text(extracted),
-        iamraw.path.textposition(extracted),
-        pages=pages,
-    )
-    result = serializeraw.load_pagenumbers(result)
-    if callable(expected):
-        expected(result)
-        return
-    if isinstance(result, tuple):
-        # left right, or multiple pages positions
-        result = result[0] + result[1]
-    assert isinstance(result, list), f' page detection type {type(result)}'
-    assert len(result) == expected
-
+ARCHIVE = utila.join(groupme.ROOT, 'tests/pagenumbers/expected', exist=True)
 
 RESOURCES = [
+    power.BACHELOR085_PDF,
+    power.BACHELOR111_PDF,
+    power.DISS148_PDF,
+    power.DISS170B_PDF,
+    power.DISS218_PDF,
     power.DISS287_PDF,
+    power.DISS406_PDF,
+    power.DISS480_PDF,
     power.HC_DISS128,
     power.HC_DISS148,
     power.HC_DISS166,
     power.HC_DISS171,
     power.HC_DISS193,
+    power.MASTER049_PDF,
+    power.MASTER072_PDF,
+    power.MASTER091A_PDF,
+    power.MASTER110_PDF,
+    power.MASTER127_PDF,
     power.PAPER14B_PDF,
     power.TECH024_PDF,
 ]
@@ -162,7 +47,7 @@ PAGENUMBERS = [pytest.param(pdf, id=utila.file_name(pdf)) for pdf in RESOURCES]
 
 @utilatest.longrun
 @pytest.mark.parametrize('source', PAGENUMBERS)
-def test_pagenumber_validate(source, monkeypatch, testdir):
+def test_validate_pagenumbers(source, monkeypatch, testdir):
     Evaluate(
         source=source,
         workdir=testdir.tmpdir,
