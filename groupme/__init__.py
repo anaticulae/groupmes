16eaf8de99646405586c07352bc09a2f7bd466b0
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-07-19 12:34:28 +0200

    0.36.1 auto generated release

--- groupme/__init__.py
+++ groupme/__init__.py
@@ -12,7 +12,7 @@ import os
 import groupme.__patch__
 import groupme.path
 
-__version__ = '0.36.0'
+__version__ = '0.36.1'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
